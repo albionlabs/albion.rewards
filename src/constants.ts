6cd743f88c9f642fd4f4740aa6c446935838308e
Siddharth2207 <siddharthbhoite22@gmail.com>
2025-12-24 12:38:44 +0530

    nov rewards

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
 export const ENERGY_FEILDS = [
-    '0xf836a500910453a397084ade41321ee20a5aade1'
+    '0xf836a500910453a397084ade41321ee20a5aade1',
+    '0x1d57246fd0ba134d7cc78ddf3ed829379d95f4b7'
 ]
